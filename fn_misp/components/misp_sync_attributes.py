5edc5d3167b3482b7319bb013ad8588521f5fb96
Hendrik Baecker <hendrik.baecker@dcso.de>
2023-11-28 15:50:43 +0100

    Post with payload

--- fn_misp/components/misp_sync_attributes.py
+++ fn_misp/components/misp_sync_attributes.py
@@ -63,7 +63,7 @@ class FunctionComponent(ResilientComponent):
 
             # Get artifacts for this incident
             log.info("Start: Gathering artifacts from API")
-            artifacts = res_client.post("/incidents/{}/artifacts/query_paged?handle_format=names".format(incident_id))
+            artifacts = res_client.post("/incidents/{}/artifacts/query_paged?handle_format=names".format(incident_id), payload={})
             log.info("Stopped: Gathering artifacts from API")
             yield StatusMessage(f"Caught {len(artifacts)} from Incident {incident_id}")
 
